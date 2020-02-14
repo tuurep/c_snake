b155df13f92f125d085d90199ec6fa5d8f915699
tuure <tuure.piitulainen@gmail.com>
2020-02-14 22:06:50 +0200

    Print stage

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
+void print_stage(int h, int w) {
+  for (int i = 0; i < h; i++) {
+    for (int j = 0; j < w; j++) {
+      printf("# ");
+    }
+    printf("\n");
+  }
+}
+
 int main() {
-   printf("Hello, world!");
-   return 0;
+  int stage_height = 10, stage_width = 10;
+  print_stage(stage_height, stage_width);
+
+  return 0;
 }
